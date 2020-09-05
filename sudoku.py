d782be51522744697f7e4d6fbaeb9cd98d2fe793
Giulio <giglio_2@hotmail.it>
2020-09-05 21:38:55 +0200

    prints whenever a solution is foud

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,4 @@
+import time
 sudoku0 = [1, 2, 3, 4, 5, 6, 7, 8, 9,
           4, 5, 6, 7, 8, 9, 1, 2, 3,
           7, 8, 9, 1, 2, 3, 4, 5, 6,
@@ -30,6 +31,14 @@ class Sudoku:
 
     def row(self, rowNumber):
         return self.sudo[9 * (rowNumber-1): 9 * rowNumber]
+    
+    def square(self, squareNumber):
+        first = 27 * ((squareNumber -1)//3) + 3 * ((squareNumber -1)%3)
+        r1 = self.sudo[first + 0: (first + 3)]
+        r2 = self.sudo[first + 9 : first + 12]
+        r3 = self.sudo[first + 18 : first + 21]
+        return r1 + r2 + r3
+        
 
     def changeName(self, num1, num2):
         indices1 = [i for i in range(81) if self.sudo[i] == num1]
@@ -80,15 +89,35 @@ class Sudoku:
         .changeColumns(3 * (columnBlock1 -1 ) + 2, 3 * (columnBlock2 -1) + 2)
         .changeColumns(3 * (columnBlock1 -1 ) + 3, 3 * (columnBlock2 -1) + 3))
 
+    def possible(self, idx, num):
+        b1 = num not in self.row((idx // 9) + 1)
+        b2 = num not in self.column((idx % 9) + 1)
+        b3 = num not in self.square(3 * ((idx // 9) //3) + ((idx % 9) //3) + 1)
+        return b1 and b2 and b3
+        
+    def solve(self):
+        for i in range(81):
+            if self.sudo[i] == 0:
+                for n in range(1,10):
+                    if self.possible(i, n):
+                        self.sudo[i] = n
+                        self.solve()
+                        self.sudo[i] = 0
+                return Sudoku(self.sudo)
+        self.printSudoku()
+        input("More?")
+
 #print(row(sudoku,1))
 s = Sudoku(sudoku)
-(s.changeColumnBlocks(1,3)
-.changeName(7,2)
-.changeName(9,3)
-.changeName(4,6)
-.changeName(8,9)
-.changeColumnBlocks(2,3)
-.changeColumns(5,6)
-.changeColumns(4,5)
-.printSudoku()
-)
+# (s.changeColumnBlocks(1,3)
+# .changeName(7,2)
+# .changeName(9,3)
+# .changeName(4,6)
+# .changeName(8,9)
+# .changeColumnBlocks(2,3)
+# .changeColumns(5,6)
+# .changeColumns(4,5)
+# .printSudoku()
+# )
+
+s.solve().printSudoku()
